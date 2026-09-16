34cfea7830c8cafafeb5257d0a24bd347afd09ce
adminlahabana <admin.lahabana@gmail.com>
2026-09-16 11:21:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LH = {
   SUPABASE_URL: "https://bhwlycvybxmgnoxmznjh.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_iFtRkET6HAi7Ei2A5ZR6rA_W6mqmd1L",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJod2x5Y3Z5YnhtZ25veG16bmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NzU2MDAsImV4cCI6MjEwNTA1MTYwMH0.1dvmzfeeZgzyoixHzRM0bq9RjRpCmUcyK5Q9_2D0qJI",
   VENUE: "La Habana"
 };
